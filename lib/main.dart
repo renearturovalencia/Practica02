78b6c325d992c272197f85cda12738ffca7345a4
rene <rvalencia7@ucol.mx>
2022-11-21 22:37:53 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+Future<Post> fetchPost() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/9'));
+
+  if (response.statusCode == 200) {
+    // Si el servidor devuelve una repuesta OK, parseamos el JSON
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    // Si esta respuesta no fue OK, lanza un error.
+    throw Exception('Failed to load post');
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _paginaActual = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Consumos de datos de internet'),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add_task),
+        ),
+        body: Center(
+          child: FutureBuilder<Post>(
+            future: fetchPost(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.title);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              // Por defecto, muestra un loading spinner
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            onTap: (index) {
+              setState(() {
+                _paginaActual = index;
+              });
+            },
+            currentIndex: _paginaActual,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Inicio"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.people_alt_sharp), label: "Usuarios"),
+            ]),
+      ),
+    );
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post(
+      {required this.userId,
+      required this.id,
+      required this.title,
+      required this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
